7e7137dd75940bde66c373e52af15c03ade9d32c
root <root@localhost.localdomain>
2015-09-25 03:02:19 -0700

    c-1

--- myweb.py
+++ myweb.py
@@ -1,13 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, make_response
+from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+
+
 apl = Flask(__name__)
+apl_manager = Manager(apl)
+bootstrap = Bootstrap(apl)
 
 @apl.route('/')
 def index():
-	return '<h1>Hello,World!</h1>'
+	return render_template('index.html')
 
 @apl.route('/user/<name>')
 def user(name):
-	return '<h1>Hello, %s</h1>' %name
+	return render_template('user.html', name=name)
+
+@apl.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html')
+
+@apl.errorhandler(500)
+def internal_server_error(e):
+	return render_template('500.html')
+
+
+
+
 
 if __name__ == '__main__':
-	apl.run(debug=True)
+	apl_manager.run()
