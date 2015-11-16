83f8fb33689c77c9f2f614f3dc7133d57cadf464
root <root@bogon.(none)>
2015-11-16 23:16:57 +0800

    Permission

--- myapp/models.py
+++ myapp/models.py
@@ -5,17 +5,43 @@ from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 
 class Permission:
-	pass
-
+	FOLLOW = 0x01
+	COMMENT = 0x02
+	WRITE_ARTICLES =0x04
+	MODERATE_COMMENTS = 0x08
+	ADMINISTER = 0x80
 
 class Role(db.Model):
 	__tablename__ = 'roles'
-	id = db.Column(db.Integer, primary_key = True)
+	id = db.Column(db.Integer, primary_key = True,index=True)
 	name = db.Column(db.String(64), unique = True)
 	users = db.relationship('User', backref='role', lazy='dynamic')
+	permission = db.Column(db.Integer)
+	default = db.Column(db.Boolean, default=False, index=True)
 		
+	
 	def __repr__(self):
 		return '<Role, %s>' % self.name
+	@staticmethod
+	def insert_roles():
+		roles={
+			'User':(Permission.FOLLOW | Permission.COMMENT | Permission.WRITE_ARTICLES , True),
+			'Moderator':(Permission.FOLLOW | Permission.COMMENT | Permission.WRITE_ARTICLES 
+						| Permission.MODERATE_COMMENTS, False),
+			'Administrator':(0xff, False)
+			}
+
+		for r in roles:
+			role= Role.query.filter_by(name=r).first()
+			if role is None:
+				role=Role(name=r)
+			role.permission=roles[r][0]
+			role.default=roles[r][1]
+			db.session.add(role)
+		db.session.commit()
+
+
+
 
 class User(UserMixin,db.Model):
 	__tablename__ = 'users'
