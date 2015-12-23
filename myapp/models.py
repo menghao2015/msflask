7062659da709482b55c67c080ceff9d351f19fd4
root <root@localhost.localdomain>
2015-12-23 17:51:11 +0800

    C_11.6_12-23_3

--- myapp/models.py
+++ myapp/models.py
@@ -54,24 +54,6 @@ class Permission:
     ADMINISTER = 0x80
 
 
-class Post(db.Model):
-	__tablename__ = 'posts'
-	id = db.Column(db.Integer, primary_key=True)
-	body = db.Column(db.Text)
-	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-	author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-	body_html = db.Column(db.Text)
-
-	@staticmethod
-	def on_changed_body(target, value, oldvalue, initiator):
-		allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
-						'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
-						'h1', 'h2', 'h3', 'p']
-		target.body_html = bleach.linkify(bleach.clean(
-							markdown(value, output_format='html'),
-							tags=allowed_tags, strip=True))
-db.event.listen(Post.body, 'set', Post.on_changed_body)
-
 
 class Role(db.Model):
 	__tablename__ = 'roles'
